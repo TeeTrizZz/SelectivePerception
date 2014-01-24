35d40e303656a9e581c54f1540dd20f75a24b8e0
Sarah Haefele <sarah.haefele@hs-furtwangen.de>
2014-01-24 23:33:06 +0100

    GUI Elements

---
+++ Assets/Scripts/GUI/GUIStart.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUIStart : MonoBehaviour {
+
+	//scene to load
+	private string levelName = "scene";
+	//start scene texture
+	public Texture startScreen;
+	//intro music
+	public AudioClip intro;
+	//GUI Style
+	private GUIStyle style;
+
+	// Use this for initialization
+	void Start () {
+		style.normal.textColor = Color.white;
+		style.fontSize = 20;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GUI.DrawTexture ( new Rect (280, 30, 400, 540), startScreen, ScaleMode.ScaleToFit, true);
+
+	}
+}
