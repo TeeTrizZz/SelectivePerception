67bee4219b7cd6330cb9a9d2a35717cd6205cd7e
Sarah Haefele <sarah.haefele@hs-furtwangen.de>
2014-01-25 13:46:42 +0100

    Skill Button mit Countdown - fehlende Tex

--- Assets/Scripts/GUI/GUIScene.cs
+++ Assets/Scripts/GUI/GUIScene.cs
@@ -6,9 +6,20 @@ public class GUIScene : MonoBehaviour {
 	//toggle state of the skill button
 	public static bool toggleState = false;
 
+	float startTime;
+
+	public Texture skill;
+
+	private int coolDown = 30;
+
+	public GUIStyle style;
+
 	// Use this for initialization
 	void Start () {
 	
+		style.normal.textColor = Color.white;
+		style.fontSize = 40;
+		style.alignment = TextAnchor.MiddleCenter; //aligns the text to middle
 	}
 	
 	// Update is called once per frame
@@ -17,6 +28,21 @@ public class GUIScene : MonoBehaviour {
 	}
 
 	void OnGUI() {
+			
+				if (GUI.Button (new Rect (Screen.width / 2, Screen.height / 2, 105, 105), skill)) {
+						if (!toggleState) {
+								toggleState = true;
+								startTime = Time.time; //save start time after clicking the button
+						}
+				}
+				//show cooldown time
+				if (toggleState) {
+					int dif = Mathf.RoundToInt(coolDown - (Time.time - startTime));
+					GUI.Label (new Rect (Screen.width / 2, Screen.height / 2, 105, 105), dif.ToString(), style);
 
-	}
+					if (dif < 0) {
+						toggleState = false;
+					}
+				}
+		}
 }
