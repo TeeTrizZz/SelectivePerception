f65441a6d15044664c6d654536bb5dc2a08ba129
Sarah Haefele <sarah.haefele@hs-furtwangen.de>
2014-01-25 12:56:09 +0100

    GUI Scene boolean angelegt

---
+++ Assets/Scripts/GUI/GUIScene.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUIScene : MonoBehaviour {
+
+	//toggle state of the skill button
+	public static boolean toggleState = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI() {
+		if (GUI.Button (new Rect(Screen.width/2 -150 ,Screen.height/2 - 30,150,30), "Start")) {
+			Application.LoadLevel (levelName);
+		}
+	}
+}
