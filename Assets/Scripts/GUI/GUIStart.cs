a65552ae3d9b2e910284777189a46dbe02d67d7c
Sarah Haefele <sarah.haefele@hs-furtwangen.de>
2014-01-25 12:23:21 +0100

    Start Gui + button

--- Assets/Scripts/GUI/GUIStart.cs
+++ Assets/Scripts/GUI/GUIStart.cs
@@ -26,6 +26,26 @@ public class GUIStart : MonoBehaviour {
 	}
 
 	void OnGUI() {
-		GUI.DrawTexture (new Rect (280, 30, 400, 540), startScreen, ScaleMode.ScaleToFit, true);
+		float width = Screen.width / 1.1f;
+		float height = Screen.height / 1.3f;
+
+		float dWidth = (Screen.width - width) / 2;
+		float dHeight = (Screen.height - height) / 2;
+
+		
+	
+		//GUI.DrawTexture (new Rect (30, 0, horizRatio, vertRatio), startScreen, ScaleMode.StretchToFill, true);
+
+		GUI.BeginGroup (new Rect (dWidth, dHeight, Screen.width, Screen.height));
+
+
+
+		GUI.DrawTexture (new Rect (0, 0, width, height), startScreen, ScaleMode.ScaleToFit, true);
+		                
+		if (GUI.Button (new Rect(Screen.width/2 -150 ,Screen.height/2 - 30,150,30), "Start")) {
+			Application.LoadLevel (levelName);
+		}
+
+		GUI.EndGroup ();
 	}
 }
