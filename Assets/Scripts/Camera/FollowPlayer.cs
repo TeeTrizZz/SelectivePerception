8221aa05212455f97f1c8c786ddfdefdfafffa9a
StefSa <ste.mueller@hs-furtwangen.de>
2014-01-25 11:51:14 +0100

    Textures

--- Assets/Scripts/Camera/FollowPlayer.cs
+++ Assets/Scripts/Camera/FollowPlayer.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class FollowPlayer : MonoBehaviour {
 	Transform cam;
 	Transform camMain;
-	Transform Char;
+	GameObject Char;
 
 	float roty;
 	float rotx;
@@ -14,17 +14,17 @@ public class FollowPlayer : MonoBehaviour {
 	void Start () {
 		camMain = transform.FindChild("Main Camera");
 		cam = this.transform;
-		Char = GameObject.Find("Player").transform;
+		Char = GameObject.FindWithTag("Player");
 
-		cam.LookAt(Char);
-		cam.position = Char.position;
+		cam.LookAt(Char.transform);
+		cam.position = Char.transform.position;
 		cam.localEulerAngles = new Vector3(0,0,0);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		cam.position = Char.position;
-		camMain.LookAt(Char);
+		cam.position = Char.transform.position;
+		camMain.LookAt(Char.transform);
 		moveCam();
 	}
 	void moveCam()
---
+++ Assets/Scripts/Player/Abilities/nightVisionGoggles.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class nightVisionGoggles : MonoBehaviour {
+
+	GameObject character;
+	GameObject text;
+
+	// Use this for initialization
+	void Start () {
+		character = this.transform.gameObject;
+		text = GameObject.Find("NightVisionElements");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void checkCharType()
+	{
+		if(character.name == "NightVisionSpark")
+		{
+			text.active = true;
+		}
+		else{
+			text.active = false;
+		}
+	}
+}
